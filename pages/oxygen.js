21a7f7613354d8619b8c49cd0e2a0affefbee829
Lalit <lalitvijay9480@gmail.com>
2021-04-28 11:13:03 +0530

    Change url of API

--- pages/oxygen.js
+++ pages/oxygen.js
@@ -3,7 +3,7 @@ import OxygenCard from "../components/OxygenCard";
 import NextLink from "next/link";
 
 export async function getStaticProps() {
-	const res = await fetch("https://iwantoxygen.vercel.app/api/allRecords");
+	const res = await fetch("https://archanamam.vercel.app/api/allRecords");
 	const records = await res.json();
 
 	return {
